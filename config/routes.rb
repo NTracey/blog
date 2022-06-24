e6f007f4ea0fb49b2c5b8257cab80a011f41470f
Tracey <traceyngo8@gmail.com>
2022-06-24 22:59:53 +1000

    fixing devise in route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-root 'articles#index', as: "home"
+  devise_for :users
+  root 'articles#index', as: "home"
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # get 'articles', to: 'articles#index'
